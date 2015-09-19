96f7e72a234456582f54f32405753085b2d2bcd4
Jacob Mortensen <jacob@yourmemoir.com>
2015-09-19 15:15:00 -0400

    input changed

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ module.exports = {
         var result = true;
         var promises = [];
         _.each(step.input('urls'), function(url) {
-            console.log(url);
-            data['url'] = url.url;
+            data['url'] = url;
             promises.push(
               rest.post("https://getpocket.com/v3/add", {data : data}).on('complete', function(result, response) {
                   try {
